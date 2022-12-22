824c209653ad1ff3b51ed690b905ab9023d5b371
HemantMangal-pec <121235953+HemantMangal-pec@users.noreply.github.com>
2022-12-22 21:20:15 +0530

    Add files via upload

---
+++ assin2 q2.py
@@ -0,0 +1,5 @@
+name = "Hemant Mangal"
+SID = "22106042"
+department = "Data Science"
+CGPA = 9.9
+print("Hey,", name, "Here!\nMy SID is", SID, "\nI am from", department, "department and my CGPA is", CGPA)
